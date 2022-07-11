1ba42ee241abf2b667ef36f2ff57d9ca553b8644
antsecv14 <antsecv14@gmail.com>
2022-07-11 15:48:43 -0500

    :art:

---
+++ htfiles/404.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>error404</title>
+	<link rel="stylesheet" type="text/css" href="/htfiles/styles.css">
+
+</head>
+
+<body>
+    <div class="con404">
+        <img src="../images/Asset1.png" alt="">
+    </div>
+</body>
+
+</html>
