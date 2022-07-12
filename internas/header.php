ad05e35096814e8c4557f914632aa7d42bebfaa7
antsecv14 <antsecv14@gmail.com>
2022-07-11 23:02:31 -0500

    :art:

---
+++ internas/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mi primera app</title>
+	<link rel="stylesheet" type="text/css" href="../css/estilos.css">
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');
+		</style>
+</head>
+
+<body>
+	<div class="headerContainer">
+		<nav class="variantes-redes">
+			<ul class="variantes">
+				<li>
+					<img src="../images/carnes_mariscos_1.png" alt="Patio" title="carnesMariscos">
+					<div class = "etiqueta">
+						<h3>
+							Carnes y Mariscos
+						</h3>
+					</div>
+				</li>
+				<li>
+					<img src="../images/cafeteria_1.png" alt="Cafeteria" title="itemCafeteria">
+					<div class = "etiqueta">
+						<h3>
+							Cafeteria
+						</h3>
+					</div>
+				</li>
+				<li>
+					<img src="../images/prado_2.png" alt="Prado" title="itemPrado">
+					<div class = "etiqueta">
+						<h3>
+							Prado
+						</h3>
+					</div>
+				</li>
+			</ul>
+			<ul class="redes">
+				<li><img class ="facebook2" src="../images/facebook2.png" alt="facebook2" title="facebook2"><img class ="facebook" src="../images/facebook.png" alt="facebook" title="facebook"></li>
+				<li><img class="instagram2" src="../images/instagram2.png" alt="instagram2" title="instagram2"><img class="instagram" src="../images/instagram.png" alt="instagram" title="instagram"></li>
+				<li><img class= "whatsap2" src="../images/whatsap2.png" alt="whatsap2" title="whatsap2"><img class= "whatsap" src="../images/whatsap.png" alt="whatsap" title="whatsap"></li>
+				<li><img class="tiktok2" src="../images/tiktok2.png" alt="tiktok2" title="tiktok2"><img class="tiktok" src="../images/tiktok.png" alt="tiktok" title="tiktok"></li>
+				<li><img class="youtube2" src="../images/youtube2.png" alt="youtube2" title="youtube2"><img class="youtube" src="../images/youtube.png" alt="youtube" title="youtube"></li>
+			</ul>
+		</nav>
+		<container class="cabeceraPrincipal">
+			<img src="../images/logo1.png">
+		</container> 
+			
+		<nav class="menuPrincipal">
+			<a href="">Inicio</a>
+			<a href="internas/productos.php">Productos</a>
+			<a href="internas/contactos.php">Registro</a>
+			<a href="internas/listados.php">Listado</a>
+			<a href="internas/login.php">Login</a>
+		</nav>
+    </div>
+
+</body>
+</html>
